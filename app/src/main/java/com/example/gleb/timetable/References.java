4d39e69ee3f3ec523e3debe747192ebc5244b7f2
GlebKalinichenko <Glebjn@yandex.ua>
2015-10-17 19:19:31 +0400

    References about faculty and university

---
+++ app/src/main/java/com/example/gleb/timetable/References.java
@@ -0,0 +1,17 @@
+package com.example.gleb.timetable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gleb on 17.10.2015.
+ */
+public class References extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_faculty);
+
+
+    }
+}
